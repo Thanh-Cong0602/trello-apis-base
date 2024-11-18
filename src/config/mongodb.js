1f0126a823e55e56803a539d5d162ecb740027c1
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-18 19:16:39 +0700

    [Lecture 45]  Create a MongoDB Atlas Cloud database with Free 512 MB package

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,3 +2,9 @@
  * Import necessary modules
  * Author: Thanh Cong Nguyen
  */
+
+// th_cong_ng
+// tcn0602C@
+
+const MONGODB_URL =
+  'mongodb+srv://th_cong_ng:tcn0602C@@cluster0-thanhcongnguye.in6kz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-ThanhCongNguyen'
