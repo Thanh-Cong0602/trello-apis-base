fca99e395dc6522289892786425317fb2a4bc492
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-19 03:23:28 +0700

    [Lecture 60] Write API Get Board Details - Initial integration into Front-end

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-catch */
 
+import { StatusCodes } from 'http-status-codes'
 import { boardModel } from '~/models/board.model'
+import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
 
 const createNew = async reqBody => {
@@ -19,6 +21,19 @@ const createNew = async reqBody => {
   }
 }
 
+const getDetails = async boardId => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+
+    if (!board) throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+
+    return board
+  } catch (error) {
+    throw error
+  }
+}
+
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
