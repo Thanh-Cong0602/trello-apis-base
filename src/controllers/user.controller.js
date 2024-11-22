866491ef0ba70d7a52559e6bbfc32954d6c55ac5
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-23 02:49:40 +0700

    [Session 9] Logout feature, automatic Token refresh with Axios Interceptors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from 'http-status-codes'
-import { userService } from '~/services/user.service'
 import ms from 'ms'
+import { userService } from '~/services/user.service'
+import ApiError from '~/utils/ApiError'
+
 const createNew = async (req, res, next) => {
   try {
     const createUser = await userService.createNew(req.body)
@@ -44,8 +46,36 @@ const login = async (req, res, next) => {
   }
 }
 
+const logout = async (req, res, next) => {
+  try {
+    res.clearCookie('accessToken')
+    res.clearCookie('refreshToken')
+    res.status(StatusCodes.OK).json({ loggedOut: true })
+  } catch (_error) {
+    next(_error)
+  }
+}
+
+const refreshToken = async (req, res, next) => {
+  try {
+    const result = await userService.refreshToken(req.cookies?.refreshToken)
+    res.cookie('accessToken', result.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    })
+
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) {
+    next(new ApiError(StatusCodes.UNAUTHORIZED, 'Please Sign In! (Error from refresh token)'))
+  }
+}
+
 export const userController = {
   createNew,
   verifyAccount,
-  login
+  login,
+  logout,
+  refreshToken
 }
